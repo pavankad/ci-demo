80c15897a0a7fcbee63fb5fe123609c3c52acdea
pavankad <40609894+pavankad@users.noreply.github.com>
2025-03-09 22:06:50 +0530

    Update generate_bid.js

--- generate_bid.js
+++ generate_bid.js
@@ -1,11 +1,7 @@
 async function generateBid(interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) {
-    // Load the Wasm helper inside the TEE
-    const response = await fetch("https://depatraining.blob.core.windows.net/wasm-code/helper.wasm?sp=r&st=2025-03-09T16:18:26Z&se=2025-05-31T00:18:26Z&spr=https&sv=2022-11-02&sr=b&sig=iIphKMPL2u%2Bt6THxDKcN5dE7qA3TT3zLowhCzPuqqSY%3D");
-    const buffer = await response.arrayBuffer();
-    const { instance } = await WebAssembly.instantiate(buffer);
 
     // Use the Wasm function for secure bid computation
-    const bidValue = instance.exports.computeBid();
+    const bidValue = computeBid();
 
     return {
         ad: interestGroup.ads[0],  // Choose the first ad from the interest group
