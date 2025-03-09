b93e3b7043f3e633c372907d777b7b16d6fbd33b
pavankad <40609894+pavankad@users.noreply.github.com>
2025-03-09 17:48:02 +0530

    Update generateBids.js

--- generateBids.js
+++ generateBids.js
@@ -2,7 +2,7 @@ function generateBids(interestGroup, auctionSignals, perBuyerSignals, trustedBid
   return {
     ad: {
       renderUrl: "https://example.com/ad",
-      metadata: { category: "test" },
+      metadata: { category: "new_test" },
       bidSignals: JSON.stringify(trustedBiddingSignals)
     },
     bid: 0.5,
