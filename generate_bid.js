4e4ff06383f0023f7232d8dd3bc7772f4f8fdb6a
pavankad <40609894+pavankad@users.noreply.github.com>
2025-03-09 22:09:47 +0530

    Update generate_bid.js

--- generate_bid.js
+++ generate_bid.js
@@ -1,7 +1,8 @@
 async function generateBid(interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) {
 
     // Use the Wasm function for secure bid computation
-    const bidValue = computeBid();
+    //const bidValue = computeBid();
+    const bidValue = 30;
 
     return {
         ad: interestGroup.ads[0],  // Choose the first ad from the interest group
