8af3a3ce25640d09bd0708e8d9dbda8e566aaf58
pavankad <40609894+pavankad@users.noreply.github.com>
2025-03-11 17:09:27 +0530

    Update generate_bid.js

--- generate_bid.js
+++ generate_bid.js
@@ -1,13 +1,9 @@
 function generateBid(interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) {
-
-    // Use the Wasm function for secure bid computation
-    //const bidValue = computeBid();
     const bidValue = 30;
-
     return {
-       ad: {
+    ad: {
       renderUrl: "https://example.com/ad",
-      metadata: { category: "new_test" },
+      metadata: { category: "test" },
       bidSignals: JSON.stringify(trustedBiddingSignals)
     },
     bid: bidValue,
