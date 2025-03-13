c0a74e91044fdd22eb31366ad3a755eab7f04e8a
pavankad <40609894+pavankad@users.noreply.github.com>
2025-03-13 21:58:21 +0530

    Update generate_bid.js

--- generate_bid.js
+++ generate_bid.js
@@ -4,10 +4,13 @@ function generateBid(interestGroup, auctionSignals, perBuyerSignals, trustedBidd
     if (!globalWasmHelper) {
            bidValue = -1;
     }else {
-    const wasmInstance = new WebAssembly.Instance(globalWasmHelper, {});    
+        const wasmInstance = new WebAssembly.Instance(globalWasmHelper, {}); 
+    }
     if (wasmInstance.exports.computeBid) {
         bidValue = wasmInstance.exports.computeBid();
         console.log("Bid Value from Wasm:", bidValue);
+    }else {
+        bidvaiue = -2;
     }
     return {
     ad: {
