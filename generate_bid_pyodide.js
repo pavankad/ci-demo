85ba0dbdca59b38f23957c6b4f9403a31af5f5ed
pavankad <40609894+pavankad@users.noreply.github.com>
2025-03-14 00:49:47 +0530

    Update generate_bid_pyodide.js

--- generate_bid_pyodide.js
+++ generate_bid_pyodide.js
@@ -1,21 +1,22 @@
-<!doctype html>
-<html>
-  <head>
-      <script src="https://cdn.jsdelivr.net/pyodide/v0.27.3/full/pyodide.js"></script>
-  </head>
-  <body>
-    Pyodide test page <br>
-    Open your browser console to see Pyodide output
-    <script type="text/javascript">
-      async function main(){
-        let pyodide = await loadPyodide();
-        console.log(pyodide.runPython(`
-            import sys
-            sys.version
-        `));
-        pyodide.runPython("print(1 + 2)");
-      }
-      main();
-    </script>
-  </body>
-</html>
+// run pyodide interactively in a web-worker
+import { loadPyodide } from "https://cdn.jsdelivr.net/pyodide/v0.27.3/full/pyodide.mjs";
+
+async function hello_python() {
+  let pyodide = await loadPyodide();
+  return pyodide.runPythonAsync("1+1");
+}
+
+async function generateBids(interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) {
+  result = hello_python().then((result) => {
+  console.log("Python says that 1+1 =", result);
+  });
+  return {
+    ad: {
+      renderUrl: "https://example.com/ad",
+      metadata: { category: "new_test" },
+      bidSignals: JSON.stringify(trustedBiddingSignals)
+    },
+    bid: result,
+    render: "https://example.com/ad"
+  };
+}
