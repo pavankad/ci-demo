5633bf656a6623aebe64561302483c30845d5fd4
pavankad <40609894+pavankad@users.noreply.github.com>
2025-03-15 01:58:01 +0530

    Update generate_bid_pyodide.js

--- generate_bid_pyodide.js
+++ generate_bid_pyodide.js
@@ -23,7 +23,7 @@ async function generateBid(interestGroup, auctionSignals, perBuyerSignals, trust
             metadata: { category: "new_test" },
             bidSignals: JSON.stringify(trustedBiddingSignals)
           },
-          bid: bid,
+          bid: 10,
           render: "https://example.com/ad"
         };
 }
